001dd8046264e77c50f49fbb0b76f9fa53991e42
oemer57 <oemer.akin@student.htldornbirn.at>
2025-02-21 16:36:52 +0100

    01_Todo_mit_REST_API started

---
+++ Javascript-Ajax/01_Todo_mit_REST_API/script.js
@@ -0,0 +1,28 @@
+LoadDatafromAPI()
+
+document.getElementById("btnLoad").addEventListener("click", function() {
+    LoadDatafromAPI();
+})
+
+function LoadDatafromAPI()
+{
+    fetch('https://67b892fc699a8a7baef48def.mockapi.io/Todo')
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json);
+        let html = [];
+
+        json.forEach(todo => {
+            html.push("<div><div>" + 
+                todo.title + 
+                "</div><img width='100px' src='" +
+                todo.image +
+                "?id=" +
+                Math.random() +
+                "'/> <div>--------</div></div>")
+        });
+
+
+        document.getElementById("content").innerHTML = html.join("");
+    });
+}
